9b2e3701255e4d4fac0b2d0ccabf4238b526bd9e
Raúl Caballero <14023048+rcaballerocode@users.noreply.github.com>
2024-06-22 12:24:38 -0500

    con with managed identity

--- azureproject/production.py
+++ azureproject/production.py
@@ -2,6 +2,7 @@ import os
 
 from .settings import *  # noqa
 from .settings import BASE_DIR
+from .get_token import get_token
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
@@ -30,15 +31,26 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password']
+    }
+}"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': conn_str_params['dbname'],
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'PASSWORD': 'set with get_token()'
     }
 }
+get_token()
 
 CACHES = {
         "default": {  
